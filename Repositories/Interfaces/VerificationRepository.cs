039e182aa3f403661b7889584d3b138916e05f14
Lucas Eduardo <lucased78@hotmail.com>
2022-08-14 07:57:46 -0300

    feat: adding VerificationRepository

---
+++ Repositories/Interfaces/VerificationRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace PhoneVerification.Repositories.Interfaces
+{
+  public interface IVerificationRepository<T>
+  {
+    public bool Save(string key, T data);
+
+    public T? GetByIdentifier(string identifier);
+
+    public Task<bool> SaveAsync(string key, T data);
+
+    public Task<T?> GetByIdentifierAsync(string identifier);
+  }
+}
---
+++ Repositories/RedisVerificationRepository.cs
@@ -0,0 +1,37 @@
+﻿using PhoneVerification.Repositories.Interfaces;
+using StackExchange.Redis;
+
+namespace PhoneVerification.Repositories
+{
+  public class RedisVerificationRepository : IVerificationRepository<string>
+  {
+    private readonly IDatabase _database;
+
+    public RedisVerificationRepository(ConnectionMultiplexer redisConnection)
+    {
+      _database = redisConnection.GetDatabase();
+    }
+
+    public string? GetByIdentifier(string identifier)
+    {
+      return _database.StringGet(identifier).ToString();
+    }
+
+    public async Task<string?> GetByIdentifierAsync(string identifier)
+    {
+      var response = await _database.StringGetAsync(identifier);
+
+      return response.ToString();
+    }
+
+    public bool Save(string key, string data)
+    {
+      return _database.SetAdd(key, data);
+    }
+
+    public Task<bool> SaveAsync(string key, string data)
+    {
+      return _database.SetAddAsync(key, data);
+    }
+  }
+}
