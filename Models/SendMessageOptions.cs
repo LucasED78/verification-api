1c7337472d635fcfa7b89aae07021865ac324246
Lucas Eduardo <lucased78@hotmail.com>
2022-08-14 07:57:26 -0300

    feat: adding models

---
+++ Models/SendMessageOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace PhoneVerification.Models
+{
+  public class SendMessageOptions
+  {
+    public string Body { get; set; } = string.Empty;
+    public string From { get; set; } = string.Empty;
+    public string To { get; set; } = string.Empty;
+  }
+}
---
+++ Models/Verification.cs
@@ -0,0 +1,8 @@
+﻿namespace Verification.Models
+{
+  public class Verification
+  {
+    public string Identifier { get; set; } = string.Empty;
+    public bool IsVerified { get; set; } = false;
+  }
+}
